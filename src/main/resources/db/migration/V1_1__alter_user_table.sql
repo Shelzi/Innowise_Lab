4918436efb8ab7bc4b8818b9018fdea340c7b831
Anton Nemtsou <nemant38@gmail.com>
2024-11-21 08:09:48 +0100

    added loger

--- src/main/resources/db/migration/V1_1__alter_user_table.sql
+++ src/main/resources/db/migration/V1_1__alter_user_table.sql
@@ -1,2 +1,6 @@
 ALTER TABLE "user"
-    ADD COLUMN age SMALLINT NOT NULL
+    ADD COLUMN age SMALLINT NOT NULL;
+    DO $$
+    BEGIN
+        PERFORM pg_sleep(30); -- Задержка в 10 секунд
+    END $$;
